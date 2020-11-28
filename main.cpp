6d01af5197f11c3d221026f4dbe161dd35dd4971
Catfish <8224169gamer@gmail.com>
2020-11-28 15:24:26 +0800

    finish greeting

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name = "";
+    cout << "please enter your name?" << endl;
+    cin >> name ;
+
+    cout << "hello " << name << endl;
+    system("pause");
+    return 0;
+}
